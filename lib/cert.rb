23d47b61752bc512c97a6950b0d9357d11deee17
Fatih Ozavci <fatih.ozavci@gamasec.net>
2013-04-30 17:07:00 +0300

    Certificate Generation - Extension Fix

--- lib/cert.rb
+++ lib/cert.rb
@@ -18,25 +18,34 @@ class Cert
     #creates a new certificate with incoming valid certificate info and with RSA key.    
     cert = OpenSSL::X509::Certificate.new
     cert.version = real_cert.version
-    cert.serial = 0
+    cert.serial = 1
     cert.subject = OpenSSL::X509::Name.new(real_cert.subject)
     cert.issuer = OpenSSL::X509::Name.new(real_cert.issuer)
     cert.public_key = key.public_key
     cert.not_before = real_cert.not_before
     cert.not_after = real_cert.not_after 
 
-    #extension certificate factory information   
-#    ef = OpenSSL::X509::ExtensionFactory.new nil,cert
-#    ef.subject_certificate = cert
-#    ef.issuer_certificate = cert
-#    cert.add_extension(ef.create_extension('basicConstraints', 'CA:TRUE', false))
-#    cert.add_extension(ef.create_extension('keyUsage', 'cRLSign,keyCertSign', true))
-#    cert.add_extension(ef.create_extension('subjectKeyIdentifier', 'hash'))
-    
     #added incoming certificate extension information to the new certificate
     real_cert.extensions.each do |extension_info|
         #puts "Extension Factory :" + extension_info
-        cert.add_extension(extension_info)
+	if ! (extension_info.to_s =~ /basicConstraints|keyUsage|subjectKeyIdentifier/)
+	puts "Real Cert - Extension: #{extension_info}"
+        cert.add_extension(extension_info) 
+	end
+    end
+
+    #extension certificate factory information   
+    ef = OpenSSL::X509::ExtensionFactory.new nil,cert
+    ef.subject_certificate = cert
+    ef.issuer_certificate = cert
+    cert.add_extension(ef.create_extension('basicConstraints', 'CA:TRUE', false))
+    cert.add_extension(ef.create_extension('keyUsage', 'cRLSign,keyCertSign', true))
+    cert.add_extension(ef.create_extension('subjectKeyIdentifier', 'hash'))
+
+    #printing fake certificate extension information
+    cert.extensions.each do |extension_info|
+        #puts "Extension Factory :" + extension_info
+	puts "Fake Cert - Extension: #{extension_info}"
     end
     
     #creating certificate signature with the RSA key and a SHA1 signature   
